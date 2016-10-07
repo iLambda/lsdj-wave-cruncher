3a28e717631f0b2345783fb4cbe053ddbde56590
Freeman <me@ilambda.io>
2016-10-07 16:16:55 +0200

    Edited scripts

--- crunch.js
+++ crunch.js
@@ -1,15 +1,13 @@
 var fs = require('fs')
 var _ = require('lodash')
 var path = require('path')
-var wav = require('node-wav')
+var cruncher = require('./lib/cruncher.js')
 
 // check usage
-if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+if (process.argv.length != 4) {
+  console.log('Usage: node crunch.js [FILENAME] [NOTE|BASE FREQUENCY]');
   process.exit(1);
 }
 
-// get buffer
-var filename = process.argv[2]
-var buffer = fs.readFileSync(filename, 'utf8')
-var data = wav.decode(buffer)
+// crunch
+cruncher(process.argv[2], process.argv[3])
