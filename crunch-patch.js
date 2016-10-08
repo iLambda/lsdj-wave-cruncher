07d723aa7ca4383cdbba8bf89c130d242bcad00c
Freeman <me@ilambda.io>
2016-10-08 04:16:57 +0200

    Added crunch-patch.js to directly patch data in a .lsdsng

---
+++ crunch-patch.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+var _ = require('lodash')
+var path = require('path')
+var cruncher = require('./lib/cruncher.js')
+
+// check usage
+if (process.argv.length != 4) {
+  console.log('Usage: node crunch.js [FILENAME] [NOTE|BASE FREQUENCY]');
+  process.exit(1);
+}
+
+// crunch
+console.log(cruncher(process.argv[2], process.argv[3]), { normalize: true })
